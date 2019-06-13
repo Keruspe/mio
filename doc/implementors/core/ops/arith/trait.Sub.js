79c3082064d39a627448a257e1096ee7b637fc1e
Deployment Bot (from Azure Pipelines) <deploy@tokio-rs.com>
2019-06-13 03:05:11 +0000

    Deploy Mio API documentation

---
+++ doc/implementors/core/ops/arith/trait.Sub.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["mio"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.Sub.html\" title=\"trait core::ops::arith::Sub\">Sub</a>&lt;<a class=\"struct\" href=\"mio/struct.PollOpt.html\" title=\"struct mio::PollOpt\">PollOpt</a>&gt; for <a class=\"struct\" href=\"mio/struct.PollOpt.html\" title=\"struct mio::PollOpt\">PollOpt</a>",synthetic:false,types:["mio::event_imp::PollOpt"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.Sub.html\" title=\"trait core::ops::arith::Sub\">Sub</a>&lt;<a class=\"struct\" href=\"mio/struct.Ready.html\" title=\"struct mio::Ready\">Ready</a>&gt; for <a class=\"struct\" href=\"mio/struct.Ready.html\" title=\"struct mio::Ready\">Ready</a>",synthetic:false,types:["mio::event_imp::Ready"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.Sub.html\" title=\"trait core::ops::arith::Sub\">Sub</a>&lt;<a class=\"struct\" href=\"mio/struct.Interests.html\" title=\"struct mio::Interests\">Interests</a>&gt; for <a class=\"struct\" href=\"mio/struct.Interests.html\" title=\"struct mio::Interests\">Interests</a>",synthetic:false,types:["mio::event_imp::Interests"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ doc/implementors/core/ops/bit/trait.BitXor.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["mio"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/bit/trait.BitXor.html\" title=\"trait core::ops::bit::BitXor\">BitXor</a>&lt;<a class=\"struct\" href=\"mio/struct.PollOpt.html\" title=\"struct mio::PollOpt\">PollOpt</a>&gt; for <a class=\"struct\" href=\"mio/struct.PollOpt.html\" title=\"struct mio::PollOpt\">PollOpt</a>",synthetic:false,types:["mio::event_imp::PollOpt"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/bit/trait.BitXor.html\" title=\"trait core::ops::bit::BitXor\">BitXor</a>&lt;<a class=\"struct\" href=\"mio/struct.Ready.html\" title=\"struct mio::Ready\">Ready</a>&gt; for <a class=\"struct\" href=\"mio/struct.Ready.html\" title=\"struct mio::Ready\">Ready</a>",synthetic:false,types:["mio::event_imp::Ready"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
